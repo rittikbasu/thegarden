ab012d48667d1d54c2a20a306eb3ae692bae5084
rittikbasu <irittik@gmail.com>
2023-11-28 04:49:56 +0530

    feat: add routing functionality to Tab comp

--- src/components/Tab.jsx
+++ src/components/Tab.jsx
@@ -1,37 +1,55 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import Link from "next/link";
+import { useRouter } from "next/router";
 import clsx from "clsx";
 
 function Tab() {
+  const router = useRouter();
   const [activeTab, setActiveTab] = useState("notes");
+
+  useEffect(() => {
+    const path = router.asPath;
+    if (path === "/") {
+      setActiveTab("notes");
+    } else if (path === "/reflect") {
+      setActiveTab("reflect");
+    } else if (path === "/search") {
+      setActiveTab("search");
+    }
+  }, [router.asPath]);
+
   return (
     <div className="flex justify-between mb-6 border border-zinc-700 rounded-xl">
-      <button
+      <Link
+        href="/"
         className={clsx(
-          "flex-1 px-4 py-2 text-sm font-medium border-r border-zinc-700",
+          "flex-1 px-4 py-2 text-sm font-medium border-r text-center border-zinc-700",
           activeTab === "notes" && "text-blue-400"
         )}
         onClick={() => setActiveTab("notes")}
       >
         notes
-      </button>
-      <button
+      </Link>
+      <Link
+        href={"/reflect"}
         className={clsx(
-          "flex-1 px-4 py-2 text-sm font-medium border-r border-zinc-700",
+          "flex-1 px-4 py-2 text-sm font-medium border-r text-center border-zinc-700",
           activeTab === "reflect" && "text-blue-400"
         )}
         onClick={() => setActiveTab("reflect")}
       >
         reflect
-      </button>
-      <button
+      </Link>
+      <Link
+        href={"/search"}
         className={clsx(
-          "flex-1 px-4 py-2 text-sm font-medium",
+          "flex-1 px-4 py-2 text-sm font-medium text-center",
           activeTab === "search" && "text-blue-400"
         )}
         onClick={() => setActiveTab("search")}
       >
         search
-      </button>
+      </Link>
     </div>
   );
 }
