05abefc3c06f52a8b4d87da24b4bc873f565ed3b
rittikbasu <irittik@gmail.com>
2023-11-30 06:38:02 +0530

    feat: add api endpoint for updating notes

---
+++ src/pages/api/updateNote.js
@@ -0,0 +1,25 @@
+import { createClient } from "@supabase/supabase-js";
+
+const SUPABASE_URL = process.env.SUPABASE_URL;
+const SUPABASE_API_KEY = process.env.SUPABASE_API_KEY;
+const supabase = createClient(SUPABASE_URL, SUPABASE_API_KEY);
+
+export default async function handler(req, res) {
+  if (req.method === "POST") {
+    const { text, id } = req.body;
+
+    const { data, error } = await supabase
+      .from("thegarden_notes")
+      .update([{ text: text }])
+      .match({ id: id });
+
+    if (error) {
+      return res.status(401).json({ error: error.message });
+    }
+
+    return res.status(200).json(data);
+  } else {
+    res.setHeader("Allow", ["POST"]);
+    res.status(405).end(`Method ${req.method} Not Allowed`);
+  }
+}
