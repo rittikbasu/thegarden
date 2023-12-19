84f23e84ccd1506b61b4800b1bef78668868b7e6
rittikbasu <irittik@gmail.com>
2023-12-20 01:22:35 +0530

    refactor: select tab based on path passed from top

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,26 +1,12 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import Link from "next/link";
-import { useRouter } from "next/router";
 import clsx from "clsx";
 
 import { FaNoteSticky } from "react-icons/fa6";
 import { IoSearch } from "react-icons/io5";
 import { RiBrainFill } from "react-icons/ri";
 
-function Tab() {
-  const router = useRouter();
-  const [activeTab, setActiveTab] = useState("notes");
-
-  useEffect(() => {
-    const path = router.asPath;
-    if (path === "/") {
-      setActiveTab("notes");
-    } else if (path === "/reflect") {
-      setActiveTab("reflect");
-    } else if (path === "/search") {
-      setActiveTab("search");
-    }
-  }, [router.asPath]);
+function Navbar({ path }) {
   return (
     <div className="fixed bottom-0 md:bottom-1 left-0 right-0 z-50 max-w-md translate-x-0 mx-auto">
       <div className="flex justify-between items-center bg-zinc-900/50 md:border md:rounded-xl border-t border-zinc-800 backdrop-blur-md">
@@ -28,9 +14,8 @@ function Tab() {
           href="/"
           className={clsx(
             "flex-1 flex flex-col items-center px-4 py-1.5 text-xs md:text-base md:py-2.5 font-medium",
-            activeTab === "notes" && "text-blue-400"
+            path === "/" && "text-blue-400"
           )}
-          onClick={() => setActiveTab("notes")}
         >
           <FaNoteSticky className="w-5 h-5 mb-1 md:hidden" />
           notes
@@ -39,9 +24,8 @@ function Tab() {
           href={"/reflect"}
           className={clsx(
             "flex-1 flex flex-col items-center px-4 py-1.5 text-xs md:text-base md:py-2.5 font-medium",
-            activeTab === "reflect" && "text-blue-400"
+            path === "/reflect" && "text-blue-400"
           )}
-          onClick={() => setActiveTab("reflect")}
         >
           <RiBrainFill className="w-5 h-5 mb-1 md:hidden" />
           reflect
@@ -50,9 +34,8 @@ function Tab() {
           href={"/search"}
           className={clsx(
             "flex-1 flex flex-col items-center px-4 py-1.5 text-xs md:text-base md:py-2.5 font-medium",
-            activeTab === "search" && "text-blue-400"
+            path === "/search" && "text-blue-400"
           )}
-          onClick={() => setActiveTab("search")}
         >
           <IoSearch className="w-5 h-5 mb-1 md:hidden" />
           search
@@ -62,4 +45,4 @@ function Tab() {
   );
 }
 
-export default Tab;
+export default Navbar;
