b7b915f877ab008e5c5bdf19879f11fda642f787
rittikbasu <irittik@gmail.com>
2024-01-09 23:56:51 +0530

    feat: update file name

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -9,7 +9,7 @@ import { useCompletion } from "ai/react";
 import Header from "@/components/Header";
 import Navbar from "@/components/Navbar";
 import Particles from "@/components/Particles";
-import AddNoteButton from "@/components/addNoteButton";
+import AddNoteButton from "@/components/NoteButton";
 import { db } from "@/utils/db";
 import { getFormattedDate } from "@/utils/formatNotes";
 import { createMessages } from "@/utils/createMessages";
