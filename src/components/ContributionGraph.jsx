636a3ef184bb955b738b136a39a6d02b80a6a185
rittikbasu <irittik@gmail.com>
2024-01-13 13:08:42 +0530

    feat: add border + update text + panel colors

--- src/components/ContributionGraph.jsx
+++ src/components/ContributionGraph.jsx
@@ -1,8 +1,15 @@
 import React, { useState, useEffect } from "react";
 import Calendar from "react-github-contribution-calendar";
 import { db } from "@/utils/db";
+import { dateToLocale } from "@/utils/formatNotes";
 
-const panelColors = ["#18181b", "#7B3F06", "#AC5808", "#F87D09", "#fb923c"];
+const panelColors = [
+  "rgb(20,20,24)",
+  "#7B3F06",
+  "#AC5808",
+  "#F87D09",
+  "#fb923c",
+];
 
 const panelAttributes = { rx: 2, ry: 2 };
 
@@ -21,10 +28,18 @@ const monthNames = [
   "dec",
 ];
 
+const monthLabelAttributes = {
+  style: {
+    fontSize: 10,
+    fill: "#a1a1aa",
+    alignmentBaseline: "central",
+  },
+};
+
 const weekLabelAttributes = {
   style: {
     fontSize: 10,
-    fill: "#aaaaaa",
+    fill: "#a1a1aa",
     alignmentBaseline: "central",
   },
 };
@@ -35,6 +50,7 @@ const ContributionGraph = () => {
   const [totalPosts, setTotalPosts] = useState(0);
   const [postsThisMonth, setPostsThisMonth] = useState(0);
   const [postsPerDay, setPostsPerDay] = useState({});
+  const today = dateToLocale(new Date());
 
   const getData = async () => {
     const result = await db.notes
@@ -46,7 +62,7 @@ const ContributionGraph = () => {
     const postsPerDay = {};
 
     result.forEach((post) => {
-      const day = new Date(post).toLocaleDateString("en-CA");
+      const day = dateToLocale(new Date(post));
       if (!postsPerDay[day]) {
         postsPerDay[day] = 0;
       }
@@ -73,31 +89,32 @@ const ContributionGraph = () => {
         <span>this month: {postsThisMonth}</span>
         <span>total: {totalPosts}</span>
       </p>
-      <div className="mt-2 mb-2">
+      <div className="border border-zinc-800/80 bg-black/30 p-2 rounded-xl  mt-2 mb-2">
         <Calendar
           values={postsPerDay}
-          until={new Date().toLocaleDateString("en-CA")}
+          until={today}
           panelColors={panelColors}
           panelAttributes={panelAttributes}
           monthNames={monthNames}
+          monthLabelAttributes={monthLabelAttributes}
           weekNames={weekNames}
           weekLabelAttributes={weekLabelAttributes}
         />
-      </div>
-      <div className="flex items-center justify-end mt-2 mr-4 md:mr-0">
-        <span className="text-xs">less</span>
-        <div className="flex mx-2">
-          {panelColors.map((color, index) => (
-            <div
-              key={index}
-              style={{ backgroundColor: color }}
-              className={`w-3 h-3 rounded-sm ${
-                index < panelColors.length - 1 ? "mr-1" : ""
-              }`}
-            ></div>
-          ))}
+        <div className="flex items-center text-zinc-400 justify-end mt-2 md:mr-4 mr-2">
+          <span className="text-xs">less</span>
+          <div className="flex mx-2">
+            {panelColors.map((color, index) => (
+              <div
+                key={index}
+                style={{ backgroundColor: color }}
+                className={`w-3 h-3 rounded-sm ${
+                  index < panelColors.length - 1 ? "mr-1" : ""
+                }`}
+              ></div>
+            ))}
+          </div>
+          <span className="text-xs">more</span>
         </div>
-        <span className="text-xs">more</span>
       </div>
     </div>
   );
