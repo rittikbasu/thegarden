c16ffaf2b37a5e2345c2549895450692210665d6
rittikbasu <irittik@gmail.com>
2024-01-13 13:10:50 +0530

    feat: change navbar on tab breakpoint

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,36 +7,36 @@ import { RiBrainFill } from "react-icons/ri";
 
 function Navbar({ path }) {
   return (
-    <div className="fixed bottom-0 font-poppins tracking-wide md:bottom-1 left-0 right-0 z-50 max-w-md translate-x-0 mx-auto">
-      <div className="flex justify-between items-center bg-zinc-950/50 md:border md:rounded-xl border-t border-zinc-800 backdrop-blur-md">
+    <div className="fixed bottom-0 font-poppins tracking-wide tab:bottom-1 left-0 right-0 z-50 max-w-md translate-x-0 mx-auto">
+      <div className="flex justify-between items-center bg-zinc-950/50 tab:border tab:rounded-xl border-t border-zinc-800/80 backdrop-blur-md">
         <Link
           href="/"
           className={clsx(
-            "flex-1 flex flex-col items-center px-4 py-1.5 text-xs md:text-base md:py-2.5 font-medium",
+            "flex-1 flex flex-col items-center px-4 py-1.5 text-xs tab:text-base tab:py-2.5 font-medium",
             path === "/" && "text-blue-400"
           )}
         >
-          <FaNoteSticky className="w-5 h-5 mb-1 md:hidden" />
+          <FaNoteSticky className="w-5 h-5 mb-1 tab:hidden" />
           notes
         </Link>
         <Link
           href={"/reflect"}
           className={clsx(
-            "flex-1 flex flex-col items-center px-4 py-1.5 text-xs md:text-base md:py-2.5 font-medium",
+            "flex-1 flex flex-col items-center px-4 py-1.5 text-xs tab:text-base tab:py-2.5 font-medium",
             path === "/reflect" && "text-blue-400"
           )}
         >
-          <RiBrainFill className="w-5 h-5 mb-1 md:hidden" />
+          <RiBrainFill className="w-5 h-5 mb-1 tab:hidden" />
           reflect
         </Link>
         <Link
           href={"/search"}
           className={clsx(
-            "flex-1 flex flex-col items-center px-4 py-1.5 text-xs md:text-base md:py-2.5 font-medium",
+            "flex-1 flex flex-col items-center px-4 py-1.5 text-xs tab:text-base tab:py-2.5 font-medium",
             path === "/search" && "text-blue-400"
           )}
         >
-          <IoSearch className="w-5 h-5 mb-1 md:hidden" />
+          <IoSearch className="w-5 h-5 mb-1 tab:hidden" />
           search
         </Link>
       </div>
