2c1bb87af94c2c71e90eb3a800299ad2642e53c3
rittikbasu <irittik@gmail.com>
2024-01-24 22:34:43 +0530

    feat: add indicator if api key not active

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,34 +1,55 @@
-import React from "react";
+import { useState, useEffect } from "react";
 import Link from "next/link";
 import clsx from "clsx";
 import { GiOrangeSlice } from "react-icons/gi";
 import { MdOutlineManageAccounts } from "react-icons/md";
 
-const Header = ({ path }) => (
-  <header className="mb-12 lg:pt-6 pt-4 font-poppins flex justify-between items-center">
-    <div className="flex items-center text-3xl font-semibold">
-      <span className="bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-400">
-        the garden
-      </span>
-      <IconWithGradient
-        IconComponent={GiOrangeSlice}
-        svgClassName="w-7 h-7 ml-2"
-      />
-    </div>
-    <Link href="/settings" passHref>
-      <div className="text-3xl">
+import { db } from "@/utils/db";
+
+const Header = ({ path }) => {
+  const [showIndicator, setShowIndicator] = useState(false);
+
+  async function checkOpenaiApiKeyStatus() {
+    const openaiApiKey = await db.settings.get("openaiApiKey");
+    if (!openaiApiKey?.text || !openaiApiKey?.status) {
+      setShowIndicator(true);
+    } else {
+      setShowIndicator(false);
+    }
+  }
+
+  useEffect(() => {
+    checkOpenaiApiKeyStatus();
+  }, [path]);
+  return (
+    <header className="mb-12 lg:pt-6 pt-4 font-poppins flex justify-between items-center">
+      <div className="flex items-center text-3xl font-semibold">
+        <span className="bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-400">
+          the garden
+        </span>
         <IconWithGradient
-          IconComponent={MdOutlineManageAccounts}
-          svgClassName="w-7 h-7"
-          iconClassName={clsx(
-            "inline-block w-7 h-7",
-            path === "/settings" && "fill-blue-400"
-          )}
+          IconComponent={GiOrangeSlice}
+          svgClassName="w-7 h-7 ml-2"
         />
       </div>
-    </Link>
-  </header>
-);
+      <Link href="/settings" passHref>
+        <div className="text-3xl relative">
+          <IconWithGradient
+            IconComponent={MdOutlineManageAccounts}
+            svgClassName="w-7 h-7"
+            iconClassName={clsx(
+              "inline-block w-7 h-7",
+              path === "/settings" && "fill-blue-400"
+            )}
+          />
+          {path !== "/settings" && showIndicator && (
+            <div className="w-2 h-2 bg-red-500 rounded-full absolute top-0 right-0"></div>
+          )}
+        </div>
+      </Link>
+    </header>
+  );
+};
 
 export default Header;
 
