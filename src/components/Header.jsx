51de6ea287fd5a8c5f5b0b9de0cea58256afb5e7
rittikbasu <irittik@gmail.com>
2024-01-27 23:54:32 +0530

    feat: add gradient to all nav icons

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,10 @@
 import { useState, useEffect } from "react";
 import Link from "next/link";
-import clsx from "clsx";
+
 import { GiOrangeSlice } from "react-icons/gi";
 import { MdOutlineManageAccounts } from "react-icons/md";
+
+import { GradientIcon } from "@/components/GradientIcon";
 
 import { db } from "@/utils/db";
 
@@ -27,20 +29,22 @@ const Header = ({ path }) => {
         <span className="bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-400">
           the garden
         </span>
-        <IconWithGradient
+        <GradientIcon
           IconComponent={GiOrangeSlice}
           svgClassName="w-7 h-7 ml-2"
+          viewBox={"0 0 30 30"}
+          gradientId="orange-slice"
         />
       </div>
       <Link href="/settings" passHref>
         <div className="text-3xl relative">
-          <IconWithGradient
+          <GradientIcon
             IconComponent={MdOutlineManageAccounts}
             svgClassName="w-7 h-7"
-            iconClassName={clsx(
-              "inline-block w-7 h-7",
-              path === "/settings" && "fill-blue-400"
-            )}
+            iconClassName="inline-block w-7 h-7"
+            viewBox={"0 0 30 30"}
+            gradientId="manage-accounts"
+            selected={path === "/settings"}
           />
           {path !== "/settings" && showIndicator && (
             <div className="w-2 h-2 bg-red-500 rounded-full absolute top-0 right-0"></div>
@@ -52,22 +56,3 @@ const Header = ({ path }) => {
 };
 
 export default Header;
-
-const IconWithGradient = ({
-  IconComponent,
-  gradientId = "icon-gradient",
-  svgClassName,
-  iconClassName,
-}) => {
-  return (
-    <svg className={svgClassName} viewBox="0 0 30 30">
-      <defs>
-        <linearGradient id={gradientId} x1="0%" y1="0%" x2="100%" y2="100%">
-          <stop offset="0%" stopColor="#E5E7EB" />
-          <stop offset="100%" stopColor="#9CA3AF" />
-        </linearGradient>
-      </defs>
-      <IconComponent fill={`url(#${gradientId})`} className={iconClassName} />
-    </svg>
-  );
-};
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,44 +1,80 @@
 import Link from "next/link";
 import clsx from "clsx";
-
 import { FaNoteSticky } from "react-icons/fa6";
 import { IoSearch } from "react-icons/io5";
 import { RiBrainFill } from "react-icons/ri";
+import { GradientIcon } from "@/components/GradientIcon";
+
+function NavLink({
+  href,
+  path,
+  IconComponent,
+  gradientId,
+  viewBox,
+  selected,
+  children,
+}) {
+  return (
+    <Link
+      href={href}
+      className="flex-1 flex flex-col items-center px-4 py-1.5 text-xs tab:text-base tab:py-2.5"
+    >
+      <GradientIcon
+        IconComponent={IconComponent}
+        svgClassName="w-5 h-5 tab:hidden mb-1"
+        iconClassName="w-5 h-5"
+        gradientId={gradientId}
+        viewBox={viewBox}
+        selected={selected}
+      />
+      <span
+        className={clsx(
+          "font-medium bg-clip-text text-transparent bg-gradient-to-b",
+          path === href
+            ? "from-blue-200 to-blue-500"
+            : "from-neutral-200 to-neutral-400"
+        )}
+      >
+        {children}
+      </span>
+    </Link>
+  );
+}
 
 function Navbar({ path }) {
   return (
     <div className="fixed bottom-0 font-poppins tracking-wide tab:bottom-1 left-0 right-0 z-50 max-w-md translate-x-0 mx-auto">
       <div className="flex justify-between bg-grid-white/[0.02] items-center bg-black/50 tab:border tab:rounded-xl border-t border-zinc-800/80 backdrop-blur-md">
-        <Link
+        <NavLink
           href="/"
-          className={clsx(
-            "flex-1 flex flex-col items-center px-4 py-1.5 text-xs tab:text-base tab:py-2.5 font-medium",
-            path === "/" && "text-blue-400"
-          )}
+          path={path}
+          IconComponent={FaNoteSticky}
+          gradientId="note-sticky"
+          viewBox={"0 0 13 13"}
+          selected={path === "/"}
         >
-          <FaNoteSticky className="w-5 h-5 mb-1 tab:hidden" />
           notes
-        </Link>
-        <Link
-          href={"/reflect"}
-          className={clsx(
-            "flex-1 flex flex-col items-center px-4 py-1.5 text-xs tab:text-base tab:py-2.5 font-medium",
-            path === "/reflect" && "text-blue-400"
-          )}
+        </NavLink>
+        <NavLink
+          href="/reflect"
+          path={path}
+          IconComponent={RiBrainFill}
+          gradientId="brain"
+          viewBox={"0 0 12 12"}
+          selected={path === "/reflect"}
         >
-          <RiBrainFill className="w-5 h-5 mb-1 tab:hidden" />
           reflect
-        </Link>
-        <Link
-          href={"/search"}
-          className={clsx(
-            "flex-1 flex flex-col items-center px-4 py-1.5 text-xs tab:text-base tab:py-2.5 font-medium",
-            path === "/search" && "text-blue-400"
-          )}
+        </NavLink>
+        <NavLink
+          href="/search"
+          path={path}
+          IconComponent={IoSearch}
+          gradientId="search"
+          viewBox={"0 0 12 12"}
+          selected={path === "/search"}
         >
-          <IoSearch className="w-5 h-5 mb-1 tab:hidden" />
           search
-        </Link>
+        </NavLink>
       </div>
     </div>
   );
